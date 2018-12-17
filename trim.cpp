73e481412fc4e7d24e42f467c0f93594e189ab33
simonHellrigl <simsi.hellrigl@gmail.com>
2018-12-17 17:00:00 +0100

    started working .h file finished

--- trim.cpp
+++ trim.cpp
@@ -10,3 +10,30 @@
  * Test functions for trim.h
  * ----------------------------------------------------------
  */
+
+ #include <string.h>
+ #include "trim.h"
+
+ void trim(const char* source, char* trimmed_string)
+ {
+   
+
+
+ }
+
+ static void get_start(const char* source, int *begin)
+ {
+   while (source[*begin] == ' ')
+   {
+     *begin = *begin+1;
+   }
+
+ }
+ static void get_end(const char* source, int *end, int *begin)
+ {
+   while (*end>=*begin && source[*end]==' ')
+   {
+     *end = *end-1;
+   }
+
+ }
--- trim.h
+++ trim.h
@@ -10,3 +10,11 @@
  * Interface for trim
  * ----------------------------------------------------------
  */
+ #ifndef ___TRIM_H
+ #define ___TRIM_H
+ #define STRLEN 16
+ void trim(const char* source, char* trimmed_string);
+ static void get_start(const char* source, int *begin);
+ static void get_end(const char* source, int *end, int *begin);
+
+ #endif
